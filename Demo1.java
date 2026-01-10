65657e341f3ba1a0acf221aa1ef10c27ab41aa4d
Deeksha kumari <deeksha.kumari_cs25@gmail.ac.in>
2026-01-10 14:37:10 +0530

    Demo1

---
+++ Demo1.java
@@ -0,0 +1,25 @@
+
+public class Demo1{
+static int x;
+int y;
+public static void m2(){
+int y=30;
+System.out.println(y);
+}
+public static void m1(){
+int y=20;
+System.out.println(y);
+m2();
+}
+public static void main(String[] args){
+int y=10;
+System.out.println("Hello World");
+System.out.println(x);
+System.out.println(y);
+m1();
+Demo1 obj = new Demo1();
+System.out.println(obj.y);
+ }
+}
+
+
