a03e1c261292731935240c050dec78e8c387f953
Deeksha kumari <deeksha.kumari_cs25@gmail.ac.in>
2026-01-14 23:18:41 +0530

    AgeOfHarry

---
+++ Basic-Programs/ProfitCalculation.java
@@ -0,0 +1,9 @@
+public class ProfitCalculation{
+    public static void main(String[] args){
+	int costprice = 129;
+	int sellingprice = 191;
+	int profit = sellingprice-costprice;
+	double profitPercentage= (profit*100)/costprice;
+	System.out.println("The cost price is INR:"+costprice+" and selling price is INR:"+sellingprice+"\nThe Profit is INR:"+profit+" and the Profit Percentage is:"+profitPercentage);
+	}
+	}
---
+++ Basic-Programs/SamAverageMarksInPCM.java
@@ -0,0 +1,10 @@
+public class SamAverageMarksInPCM{
+    public static void main(String[] args){
+	int Maths=94;
+	int Physics=95;
+	int Chemistry=96;
+	double average=(Maths+Physics+Chemistry)/3;
+	System.out.println("Sam's average mark in PCM:"+average);
+	}
+	}
+	
