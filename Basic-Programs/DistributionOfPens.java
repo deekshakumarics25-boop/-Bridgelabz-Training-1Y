135d49592b0022a95acda739dc947853d829f254
Deeksha kumari <deeksha.kumari_cs25@gmail.ac.in>
2026-02-02 16:44:31 +0530

    arrays

--- Basic-Programs/DistributionOfPens.java
+++
@@ -1,7 +0,0 @@
-public class DistributionOfPens{
-public static void main(String[] args){
- double distributedPens = 14/3;
- double nondistributedPens = 14%3;
- System.out.println("The Pen Per Student is :"+distributedPens+" and the remaining pen not distributed:"+nondistributedPens);
- }
- }
