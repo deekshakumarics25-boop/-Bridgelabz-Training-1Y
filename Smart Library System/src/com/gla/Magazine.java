ce8acaed1a572d9095a0265d640338ad076fb9e2
Deeksha kumari <deeksha.kumari_cs25@gmail.ac.in>
2026-03-01 19:08:55 +0530

    [Deeksha kumari] Smart Library System

---
+++ Smart Library System/src/com/gla/Magazine.java
@@ -0,0 +1,12 @@
+package com.gla;
+
+public class Magazine extends Book {
+    Magazine(int bookId, String title, double price) {
+        super(bookId, title, price);
+    }
+
+    @Override
+    double calculateFine(int daysLate) {
+        return daysLate * 5;
+    }
+}
